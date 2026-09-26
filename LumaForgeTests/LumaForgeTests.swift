cb0097a36c335e0193abf321d8a10c77b3746fd4
SIKEØ <157708940+ITZproVenom@users.noreply.github.com>
2026-09-26 21:16:24 +0530

    Stage 2: implement Steam, Workshop, package inspection, and export pipeline

--- LumaForgeTests/LumaForgeTests.swift
+++ LumaForgeTests/LumaForgeTests.swift
@@ -3,15 +3,8 @@ import XCTest
 
 final class LumaForgeTests: XCTestCase {
     func testSteamIDValidation() {
-        XCTAssertTrue(SteamOpenIDValidator.isValidSteamID("76561198000000000"))
-        XCTAssertFalse(SteamOpenIDValidator.isValidSteamID("123"))
-        XCTAssertFalse(SteamOpenIDValidator.isValidSteamID("abc"))
-    }
-
-    func testBinaryAssetScannerFindsPNG() throws {
-        let png = Data([0x89,0x50,0x4E,0x47,0x0D,0x0A,0x1A,0x0A,0x01,0x02,0x03])
-        let assets = BinaryAssetScanner.scan(png)
-        XCTAssertEqual(assets.first?.kind, .png)
-        XCTAssertEqual(assets.first?.data.count, png.count)
+        let callback=URL(string:"lumaforge://steam-callback?state=test&openid.mode=id_res&openid.op_endpoint=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Flogin&openid.claimed_id=https%3A%2F%2Fsteamcommunity.com%2Fprofiles%2F76561198000000000")!
+        XCTAssertEqual(SteamOpenID.steamID(from:callback,expectedState:"test"),"76561198000000000")
+        XCTAssertNil(SteamOpenID.steamID(from:callback,expectedState:"wrong"))
     }
 }
