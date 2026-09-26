e0b9bb0738d2c89bd7b86113a7bfa3b9577b367a
SIKEØ <157708940+ITZproVenom@users.noreply.github.com>
2026-09-26 21:19:07 +0530

    Stage 4: add LumaForgeTests/LumaForgeTests.swift

---
+++ LumaForgeTests/LumaForgeTests.swift
@@ -0,0 +1,5 @@
+import XCTest
+@testable import LumaForge
+final class LumaForgeTests:XCTestCase{
+ func testSteamIDValidation(){let u=URL(string:"lumaforge://steam-callback?state=test&openid.mode=id_res&openid.op_endpoint=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Flogin&openid.claimed_id=https%3A%2F%2Fsteamcommunity.com%2Fprofiles%2F76561198000000000")!;XCTAssertEqual(SteamOpenID.steamID(from:u,expectedState:"test"),"76561198000000000");XCTAssertNil(SteamOpenID.steamID(from:u,expectedState:"bad"))}
+}
